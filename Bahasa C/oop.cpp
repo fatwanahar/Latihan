0e50e6150b25b05f264a1798bea6452e5aaf5332
fatwanahar <fatwanahar@gmail.com>
2022-02-15 19:19:03 +0700

    menambahkan nilai masing2 pembangun prisma

--- Bahasa C/oop.cpp
+++ Bahasa C/oop.cpp
@@ -1,12 +1,28 @@
 #include<stdio.h>
-struct prisma
-{
-    int balok,segitiga;
-};
-void prisma(){
+    struct segiempat
+    {
+        int panjang,lebar;
+    };
 
-}
+    struct segitiga
+    {
+        int alas,tinggi;
+    };
+
+    struct prisma
+    {
+        struct segiempat persegi;
+        struct segitiga segi3;
+    };
+    void hitung(){
+        
+    }
 
 main(){
+    struct prisma prisma1;
+    prisma1.persegi.panjang = 1;
+    prisma1.persegi.lebar = 2;
+    prisma1.segi3.alas = 4;
+    prisma1.segi3.tinggi = 4;
 
 }
