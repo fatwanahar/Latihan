f4ab8c5a998d7e4cc90a21356c88fdbdfe51dabb
fatwanahar <fatwanahar@gmail.com>
2022-02-18 13:07:21 +0700

    menambahkan main dan struct

--- Bahasa C/oop volume prisma segitiga.cpp
+++ Bahasa C/oop volume prisma segitiga.cpp
@@ -1,5 +1,23 @@
 #include<stdio.h>
-struct LuasAlas
+struct luasAlas
 {
-    int p,a;
+    int t,a;
 };
+struct prisma
+{
+    struct luasAlas alas;
+    int t;
+};
+struct prisma prisma1;
+void Luas(){
+    
+}
+main(){
+    int v;
+    prisma1.alas.t=5;
+    prisma1.alas.a=10;
+    prisma1.t=5;
+}
+
+
+
