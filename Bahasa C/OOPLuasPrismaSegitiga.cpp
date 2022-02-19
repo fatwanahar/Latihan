1023c9abab1c3335c89e70238672a042c976216a
fatwanahar <fatwanahar@gmail.com>
2022-02-19 08:14:32 +0700

    menambah fungsi penghitungan total luas permukaan

--- Bahasa C/OOPLuasPrismaSegitiga.cpp
+++ Bahasa C/OOPLuasPrismaSegitiga.cpp
@@ -13,19 +13,19 @@
         struct segitiga segi3;
     };
     void hitungPersegi(int *n,int p,int l){
-        struct prisma prisma1;
         *n=p*l;
         return;
     }
     void hitungSegitiga(int *m,int a,int t){
-        struct prisma prisma1;
         *m = (a*t)/2;
         return;
     }
-
+    void LuasPrisma(int *L,int n,int m){
+        *L=(3*n)+(2*m);
+    }
 main(){
     struct prisma prisma1;
-    int n,m,p,l,a,t;
+    int n,m,p,l,a,t,L;
     prisma1.persegi.panjang = 2;
     prisma1.persegi.lebar = 2;
     prisma1.segi3.alas = 4;
@@ -36,6 +36,8 @@ main(){
     t=prisma1.segi3.tinggi;
     hitungPersegi(&n,p,l);
     hitungSegitiga(&m,a,t);
+    LuasPrisma(&L,n,m);
     printf("panjang segiempat: %i\n",n);
-    printf("panjang segitiga: %i",m);
+    printf("panjang segitiga: %i\n",m);
+    printf("Luas permukaan prisma adalah: %i",L);
 }
