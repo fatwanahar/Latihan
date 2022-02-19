d9bcaada6191e26dbca7c7656f0b1cbc5734595e
fatwanahar <fatwanahar@gmail.com>
2022-02-19 22:35:17 +0700

    menambah inputan user

--- Bahasa C/c++.cpp
+++ Bahasa C/c++.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include<string>
 using namespace std;
 
 int main() {
-  cout << "Hello World!";
-  return 0;
+    int a,b,c,d;
+    string nama;
+   /* string nama = "fatwa Sifaun Nahar";
+    a=1;
+    b=2;
+    c=3;
+    d=4;
+    cout << a << "\n";
+    cout << "Namaku adalah " << nama << "\n";
+    cout << "halo apa kabar kalian semua!" << endl;
+    cout << "Halo juga apa kabar!" << endl;*/
+    cout << "masukkan namamu: ";
+    cin >> nama;
+    cout << "Halo " << nama << "\n";
+    cout << "jumlah karakter dalam namamu ada: " << nama.size();
+    return 0;
 }
 
