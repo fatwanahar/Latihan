c77d9d43bd967c5ac1e4f3e34bcdd708844f3993
fatwanahar <fatwanahar@gmail.com>
2022-02-19 08:07:32 +0700

    melengkapi function dan menyelesaikan program

--- Bahasa C/OOPVolumePrismaSegitiga.cpp
+++ Bahasa C/OOPVolumePrismaSegitiga.cpp
@@ -9,14 +9,24 @@ struct prisma
     int t;
 };
 struct prisma prisma1;
-void Luas(){
-    
+void LuasAlas(int *la,int at,int aa){
+    *la=(at*aa)/2;
+}
+void Volume(int *v,int la,int t){
+    *v=la*t;
 }
 main(){
-    int v;
+    int v,la,at,aa,t;
     prisma1.alas.t=5;
     prisma1.alas.a=10;
     prisma1.t=5;
+    at=prisma1.alas.t;
+    aa=prisma1.alas.a;
+    t=prisma1.t;
+    LuasAlas(&la,at,aa);
+    //printf("%i",la);
+    Volume(&v,la,t);
+    printf("%i",v);
 }
 
 
