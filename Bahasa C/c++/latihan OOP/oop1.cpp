9cbc0cca9789e07957124fa277b09ee3758b5710
fatwanahar <fatwanahar@gmail.com>
2022-03-07 19:58:10 +0700

    menambah cout

--- Bahasa C/c++/latihan OOP/oop1.cpp
+++ Bahasa C/c++/latihan OOP/oop1.cpp
@@ -15,6 +15,9 @@ int main(int argc, char const *argv[])
     mahasiswa1.jurusan="Teknik Informatika";
     mahasiswa1.npm="200810100723";
     mahasiswa1.ipk=3.14;
-    cout << mahasiswa1.nama << endl;
+    cout << "nama mahasiswa 1:" << mahasiswa1.nama << endl;
+    cout << "npm mahasiswa 1: " << mahasiswa1.npm << endl;
+    cout << "jurusan mahasiswa 1: " << mahasiswa1.jurusan << endl;
+    cout << "IPK mahasiswa1: " << mahasiswa1.ipk << endl;
     return 0;
 }
