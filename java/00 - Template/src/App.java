8d790e7ed53a94b337dafb3a886b9015ff60aa85
fatwanahar <fatwanahar@gmail.com>
2022-04-08 18:30:49 +0700

    menambahkan file java

---
+++ java/00 - Template/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        int a,b;
+        a=4;
+        System.out.println("angka dalam variabel a adalah " + a);
+        b=5;
+        System.out.printf("angka dalam variabel b adalah %d",b);
+    }
+}
