54339b40a86c251fbcefd76c5954a64079a6329f
fatwanahar <fatwanahar@gmail.com>
2022-04-11 19:03:09 +0700

    menambahkan kode memunculkan bintang loop

--- java/04 - Loop/src/com/latihan/Main.java
+++ java/04 - Loop/src/com/latihan/Main.java
@@ -1,7 +1,50 @@
 package com.latihan;
-
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner inputUser = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        int a=inputUser.nextInt();
+        for(int i=0;i<a;i++){
+            for (int j=0;j<a;j++){
+                System.out.printf("* ");
+                if(j==i){
+                    break;
+                }
+            }
+            System.out.println("");
+        }
+        for(int i=0;i<a;i++){
+            for (int j=0;j<a;j++){
+                System.out.printf("* ");
+                if(j+i==a-1){
+                    break;
+                }
+            }
+            System.out.println("");
+        }
+        for(int i=0;i<a;i++){
+            for (int j=0;j<a;j++){
+                if (j>=a-i-1){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.printf("  ");
+                }
+            }
+            System.out.println("");
+        }
+        for(int i=0;i<a;i++){
+            for (int j=0;j<a;j++){
+                if (j>=i){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.printf("  ");
+                }
+            }
+            System.out.println("");
+        }
+        inputUser.close();
     }
 }
