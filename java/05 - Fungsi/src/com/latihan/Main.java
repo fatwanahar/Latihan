b73b250233da9c6de2658091824635c2d9c9467b
fatwanahar <fatwanahar@gmail.com>
2022-04-11 20:11:46 +0700

    menambahkan switch+fungsi

--- java/05 - Fungsi/src/com/latihan/Main.java
+++ java/05 - Fungsi/src/com/latihan/Main.java
@@ -3,17 +3,59 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         int a,b,c;
+        char operator;
         Scanner inputUser = new Scanner(System.in);
+        //angka pertama
         System.out.print("Masukkan Angka pertama: ");
         a=inputUser.nextInt();
+
+        //operator
+        System.out.print("masukkan Operator: ");
+        operator=inputUser.next().charAt(0);
+
+        //angka kedua
         System.out.printf("Masukkan Angka kedua: ");
         b=inputUser.nextInt();
-        c=penjumlahan(a,b);
-        System.out.printf("%d + %d = %d",a,b,c);
+
+        //pemanggilan fungsi
+        switch (operator) {
+            case '+':
+                c =penjumlahan(a, b); 
+                System.out.printf("%d %c %d = %d",a,operator,b,c);
+                break;
+            case '-':
+                c =pengurangan(a, b);
+                System.out.printf("%d %c %d = %d",a,operator,b,c);
+                break;
+            case '*':
+                c =perkalian(a, b);
+                System.out.printf("%d %c %d = %d",a,operator,b,c);
+                break;
+            case '/':
+                c =pembagian(a, b);
+                System.out.printf("%d %c %d = %d",a,operator,b,c);
+                break;
+            default:
+                System.out.println("salah memasukkan operator");
+                break;
+        }
         inputUser.close();
     }
     private static int penjumlahan(int a, int b) {
         int c=a+b;
         return c;
     }
+    private static int pengurangan(int a, int b) {
+        int c=a-b;
+        return c;
+    }
+    private static int perkalian(int a, int b) {
+        int c=a*b;
+        return c;
+    }
+    private static int pembagian(int a, int b) {
+        int c=a/b;
+        return c;
+    }
+
 }
