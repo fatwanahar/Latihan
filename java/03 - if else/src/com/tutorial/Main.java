ea586ce860fe473a3eafa9f2b6418d1a18a85a9b
fatwanahar <fatwanahar@gmail.com>
2022-04-11 18:14:49 +0700

    menambahkan baris kode if else

--- java/03 - if else/src/com/tutorial/Main.java
+++ java/03 - if else/src/com/tutorial/Main.java
@@ -1,8 +1,29 @@
 package com.tutorial;
-
+import java.time.LocalTime;
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("HAIiiiiiiiii");
+        int time;
+        LocalTime waktu = LocalTime.now();
+        System.out.println("Waktu Menunjukkan pukul "+waktu +" WIB");
+        Scanner inputUser = new Scanner(System.in);
+        System.out.printf("Masukkan Waktu(0-23): ");
+        time = inputUser.nextInt();
+        System.out.printf("Sekarang Pukul %d:00 WIB \n",time);
+        if(time>=0 && time<=9){
+            System.out.println("Selamat Pagi!");
+        }
+        else if(time>9 && time<=16) {
+            System.out.println("Selamat Siang!");
+        }
+        else if(time>16 && time<=19){
+            System.out.println("Selamat Sore!");
+        }
+        else if (time>19 && time<24){
+            System.out.println("Selamat Malam!");
+        }
+        else{
+            System.out.println("Salah Input!");
+        }
     }
 }
