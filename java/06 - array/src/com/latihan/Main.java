7ef6ad7b919f1f35ca68e337ff3b7729a21e7eb0
fatwanahar <fatwanahar@gmail.com>
2022-04-12 18:14:23 +0700

    menabahkan beberapa utility array

--- java/06 - array/src/com/latihan/Main.java
+++ java/06 - array/src/com/latihan/Main.java
@@ -4,16 +4,29 @@ public class Main {
     public static void main(String[] args) {
         //print array
         int[] array1 = {1,2,3,4,5,6,7,8,9,10};
-        printarray(array1);
+        printarray(array1,"array 1");
 
         //reference array
         int [] array2 = new int[array1.length];
         array2=array1;
-        printarray(array1);
-        printarray(array2);
+        printarray(array1,"array 1");
+        printarray(array2,"array 2");
+
+        //operator array/utility
+        System.out.println("\nMengkopi array dengan copyof");
+        int[]array3 = new int [array1.length];
+        array3 = Arrays.copyOf(array1, array1.length);
+        printarray(array3, "array 3");
+        System.out.println("memory array1 = "+array1);
+        System.out.println("memory array2 = "+array2);
+        System.out.println("memory array3 = "+array3);
+        
+        // for(int i=0;i<array1.length;i++){
+        //     System.out.print(array1[i]);
+        // }
     }
-    private static void printarray(int[] array1) {
-        System.out.println(Arrays.toString(array1));
+    private static void printarray(int[] array1,String a) {
+        System.out.println(a+ " : " +Arrays.toString(array1));
     }
     
 }
